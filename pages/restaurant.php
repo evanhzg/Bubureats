f7030f585e30d186ee5741088daa9685e9b1bb28
evanhzg <pv.evaan@gmail.com>
2021-01-23 21:26:26 +0100

    présentation des plats,couleur de fond et oranisation des cartes

--- pages/restaurant.php
+++ pages/restaurant.php
@@ -2,4 +2,12 @@
     <?php
     echo parse('restaurant-header.html', $restaurant);
     ?>
+    <h2>Nos plats</h2>
+    <div class="row liste-plats">
+        <?php
+        foreach($plats as $plat){
+            echo parse('plats-small.html', $plat);
+        }
+        ?>
+    </div>
 </div>
--- pages/restaurants.php
+++ pages/restaurants.php
@@ -1,10 +1,9 @@
 <div class="container">
     <h1>Liste des restaurants</h1>
-    <div class="card-deck">
+    <div class="row">
         <?php
         foreach($restaurants as $restaurant){
             echo parse('restaurant-small.html', $restaurant);
-            //echo '<div><a href="index.php?page=restaurant&restaurant_id=' . $restaurant['id'] . '">' . $restaurant['nom'] . '</a></div>';
         }
         ?>
     </div>
